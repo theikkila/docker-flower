ab42ed8263e22cc1e067ae3505ca34fb31b140f8
Nolan Nichols <nolan.nichols@gmail.com>
2014-08-24 13:10:53 -0700

    fix: env variables set to MQ

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export BROKER=--broker=amqp://${DB_PORT_5672_TCP_ADDR}:${DB_PORT_5672_TCP_PORT}//
-export BROKER_API=--broker_api=http://guest:guest@${DB_PORT_15672_TCP_ADDR}:${DB_PORT_15672_TCP_PORT}/api/
+export BROKER=amqp://${MQ_PORT_5672_TCP_ADDR}:${MQ_PORT_5672_TCP_PORT}//
+export BROKER_API=http://guest:guest@${MQ_PORT_15672_TCP_ADDR}:${MQ_PORT_15672_TCP_PORT}/api/
 
-/usr/local/miniconda/bin/flower ${BROKER} ${BROKER_API}
+/usr/local/miniconda/bin/flower --broker=${BROKER} --broker_api=${BROKER_API}
