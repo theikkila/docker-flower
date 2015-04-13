d09e9d8fe130a89b92110b025320585f9ef9777f
Teemu Heikkilä <sienikasvusto@pistoke.org>
2015-04-13 17:27:27 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-export BROKER=amqp://${MQ_PORT_5672_TCP_ADDR}:${MQ_PORT_5672_TCP_PORT}//
-export BROKER_API=http://guest:guest@${MQ_PORT_15672_TCP_ADDR}:${MQ_PORT_15672_TCP_PORT}/api/
-
-/usr/local/miniconda/bin/flower --broker=${BROKER} --broker_api=${BROKER_API}
+/usr/local/miniconda/bin/flower --broker=${BROKER}
